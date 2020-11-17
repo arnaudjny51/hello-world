a5a01f7a20ddd53ef25b43a6892f79073e686df4
Arnaud Jacquin <arnaudjny@gmail.com>
2020-11-17 22:00:14 +0000

    add hello.py (main), myname.py (function), .gitignore

---
+++ hello.py
@@ -0,0 +1,26 @@
+# hello.py
+import myname
+"""
+Created on Mon Nov 16 17:15:42 2020
+
+@author: AJVZ
+"""
+# Sample file to test functionality of Git/GitHub
+# Using tutorial: Intro to Git and GitHub for Python developers
+# https://realpython.com/python-git-github-intro/#what-is-git
+
+print("hello Git!")
+
+name = myname.get_name()
+print("hello {}".format(name))  # greats me by name
+
+
+
+
+
+
+
+
+
+
+
---
+++ myname.py
@@ -0,0 +1,11 @@
+# myname.py
+"""
+Created on Mon Nov 16 17:44:00 2020
+
+@author: AJVZ
+"""
+def get_name():
+    return "arnaud"
+
+
+
